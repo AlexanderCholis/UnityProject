69c6ddad2721fc55b14bb352f8067102bd29d5c8
Helen Polychroni <p20161@cslab.unipi.gr>
2024-01-19 12:30:33 +0200

    add resume menu

---
+++ Unity Project/Assets/PauseMenu.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // back to normal
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        //Debug.Log("Loading Menu...");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+
+    }
+}
