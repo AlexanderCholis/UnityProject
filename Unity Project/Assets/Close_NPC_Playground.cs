9501abaf9056d081f268910e1e1fd7b9921949a3
Helen Polychroni <p20161@cslab.unipi.gr>
2024-02-02 01:33:00 +0200

    add message to NPC in playground

--- Unity Project/Assets/Close_NPC_Playground.cs
+++ Unity Project/Assets/Close_NPC_Playground.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Close_NPC_Playground : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string npcMessage = "Hello there, collect all the black objcts\n" +
+        "located in the playground in order to solve the riddle!";
+    public Canvas dialogueCanvas; // Link this in the Unity Editor
+    private TMPro.TextMeshProUGUI dialogueText;
+   
     void Start()
     {
-        
+        // Assuming the Text component is a child of the Canvas
+        //dialogueText = dialogueCanvas.GetComponentInChildren<Text>();
+        dialogueText = dialogueCanvas.transform.Find("Text1").GetComponent<TextMeshProUGUI>();
+
+        if (dialogueText == null)
+        {
+            Debug.LogError("Text component not found in the Canvas!");
+        }
+        else
+        {
+            dialogueCanvas.enabled = false;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            if (dialogueText != null)
+            {
+                dialogueText.text = npcMessage;
+            }
+            else
+            {
+                Debug.LogError("Text component not found in the Canvas!");
+            }
+
+            dialogueCanvas.enabled = true;
+        }
+        else
+        {
+            Debug.LogError("Error on trigger enter");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dialogueCanvas.enabled = false;
+        }
+        else
+        {
+            Debug.LogError("Error on trigger exit");
+        }
     }
 }
