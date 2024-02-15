3c5c325cb8806cf669be792a3b8178a4810b6027
Alexander Cholis <alexholis2@gmail.com>
2024-02-15 21:16:20 +0200

    Quiz added on the Parking v1

---
+++ Unity Project/Assets/Manager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject[] Levels;
+
+    int currentLevel;
+
+    public void correctAnswer()
+    {
+        if (currentLevel + 1 != Levels.Length)
+        {
+            Levels[currentLevel].SetActive(false); 
+            currentLevel++;
+            Levels[currentLevel].SetActive(true);
+        }
+    }
+}
