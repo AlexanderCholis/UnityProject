b97ac200c4a5c0bbe5fa9a97463554a2ddfea6b5
Alexander Cholis <alexholis2@gmail.com>
2024-02-17 15:25:08 +0200

    Quiz added to the Parking v4

--- Unity Project/Assets/Manager.cs
+++ Unity Project/Assets/Manager.cs
@@ -2,54 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCInteraction : MonoBehaviour
+public class Manager : MonoBehaviour
 {
-    public GameObject[] Levels; // Array of UI elements representing different levels
-    private int currentLevel = 0; // Variable to keep track of the current level
-    public GameObject Canvas2; // Reference to the canvas GameObject
+    public GameObject[] Levels;
 
-    private void Start()
+    int currentLevel;
+
+    public void correctAnswer()
     {
-        // Deactivate all level UI elements except the first one when the game starts
-        for (int i = 1; i < Levels.Length; i++)
+        if (currentLevel + 1 != Levels.Length)
         {
-            Levels[i].SetActive(false);
-        }
-
-        // Deactivate the canvas when the game starts
-        Canvas2.SetActive(false);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player")) // Assuming your main character has the "Player" tag
-        {
-            // Show the UI element corresponding to the current level
-            Levels[currentLevel].SetActive(true);
-
-            // Activate the canvas
-            Canvas2.SetActive(true);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player")) // Assuming your main character has the "Player" tag
-        {
-            // Hide the UI element corresponding to the current level
             Levels[currentLevel].SetActive(false);
-
-            // Deactivate the canvas when the player exits the trigger
-            Canvas2.SetActive(false);
+            currentLevel++;
+            Levels[currentLevel].SetActive(true);
         }
-    }
-
-    public void SetCurrentLevel(int level)
-    {
-        // Update the current level
-        currentLevel = level;
-        // Show the UI element corresponding to the current level
-        Levels[currentLevel].SetActive(true);
     }
 }
-
--- Unity Project/Assets/NPCInteraction.cs
+++ Unity Project/Assets/NPCInteraction.cs
@@ -5,18 +5,10 @@ using UnityEngine.UI;
 
 public class NPCInteraction : MonoBehaviour
 {
-    public GameObject[] Levels; // Array of UI elements representing different levels
-    private int currentLevel = 0; // Variable to keep track of the current level
     public GameObject Canvas2; // Reference to the canvas GameObject
 
     private void Start()
     {
-        // Deactivate all level UI elements except the first one when the game starts
-        for (int i = 1; i < Levels.Length; i++)
-        {
-            Levels[i].SetActive(false);
-        }
-
         // Deactivate the canvas when the game starts
         Canvas2.SetActive(false);
     }
@@ -25,11 +17,7 @@ public class NPCInteraction : MonoBehaviour
     {
         if (other.CompareTag("Player")) // Assuming your main character has the "Player" tag
         {
-            // Show the UI element corresponding to the current level
-            Levels[currentLevel].SetActive(true);
-
-            // Activate the canvas
-            Canvas2.SetActive(true);
+            Canvas2.SetActive(true); // Show the canvas when the player enters the trigger
         }
     }
 
@@ -37,24 +25,8 @@ public class NPCInteraction : MonoBehaviour
     {
         if (other.CompareTag("Player")) // Assuming your main character has the "Player" tag
         {
-            // Hide all level UI elements when the player exits the trigger
-            foreach (GameObject uiElement in Levels)
-            {
-                uiElement.SetActive(false);
-            }
-            // Deactivate the canvas when the player exits the trigger
-            Canvas2.SetActive(false);
-        }
-    }
-
-    public void SetCurrentLevel(int level)
-    {
-        // Update the current level
-        currentLevel = level;
-        // Hide all level UI elements except the one corresponding to the current level
-        for (int i = 0; i < Levels.Length; i++)
-        {
-            Levels[i].SetActive(i == currentLevel);
+            Canvas2.SetActive(false); // Hide the canvas when the player exits the trigger
         }
     }
 }
+
