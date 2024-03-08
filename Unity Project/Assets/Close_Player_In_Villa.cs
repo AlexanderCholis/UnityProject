9a879fa78d059176b0b79136ea74ffc824192e88
Helen Polychroni <p20161@cslab.unipi.gr>
2024-03-08 15:20:22 +0200

    add objects in villa (medium)

---
+++ Unity Project/Assets/Close_Player_In_Villa.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Close_Player_In_Villa : MonoBehaviour
+{
+     public string npcMessage = "Hello there, you have been assigned a very important role.\n" +
+        "You are a detective investigating a murder case. Search the house and collect the" +
+        " \n7 objects that incriminate him.The objects are scattered in the different areas" +
+        "of the house. Good luck";
+
+    public Canvas dialogueCanvas; // Link this in the Unity Editor
+    private TextMeshProUGUI dialogueText;
+
+    void Start()
+    {
+        // Assuming the Text component is a child of the Canvas
+        //dialogueText = dialogueCanvas.GetComponentInChildren<Text>();
+        dialogueText = dialogueCanvas.transform.Find("Text").GetComponent<TextMeshProUGUI>();
+
+        if (dialogueText == null)
+        {
+            Debug.LogError("Text component not found in the Canvas!");
+        }
+        else
+        {
+            dialogueCanvas.enabled = false;
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (dialogueText != null)
+            {
+                dialogueText.text = npcMessage;
+            }
+            else
+            {
+                Debug.LogError("Text component not found in the Canvas!");
+            }
+
+            dialogueCanvas.enabled = true;
+        }
+        else
+        {
+            Debug.LogError("Error on trigger enter");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dialogueCanvas.enabled = false;
+        }
+        else
+        {
+            Debug.LogError("Error on trigger exit");
+        }
+    }
+
+
+}
