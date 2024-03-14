c72b6f5afcd6b92fefcc8f0a1dac85af9779c7c3
Helen Polychroni <p20161@cslab.unipi.gr>
2024-03-14 14:02:09 +0200

    use playerpref for score

--- Unity Project/Assets/Close_Gun_Villa_Medium.cs
+++ Unity Project/Assets/Close_Gun_Villa_Medium.cs
@@ -7,18 +7,22 @@ using TMPro;
 public class Close_Gun_Villa : MonoBehaviour
 {
     public string npcMessage = "You found the gun, keep going!";
+    public string npcMessage1 = "You found all the hidden objects!";
     public Canvas dialogueCanvas; // Link this in the Unity Editor
     private TextMeshProUGUI dialogueText;
 
     public GameObject BlackGun;
 
-   
+    public int totalHiddenObjects = 1;  // include terrified woman too
+    public int hiddenObjectCount = 0; // Counter for hidden objects
+
+
     void Start()
     {
         // Assuming the Text component is a child of the Canvas
         //dialogueText = dialogueCanvas.GetComponentInChildren<Text>();
         dialogueText = dialogueCanvas.transform.Find("Text").GetComponent<TextMeshProUGUI>();
-
+       
         if (dialogueText == null)
         {
             Debug.LogError("Text component not found in the Canvas!");
@@ -35,6 +39,10 @@ public class Close_Gun_Villa : MonoBehaviour
         yield return new WaitForSeconds(delay);
         if (BlackGun != null)
         {
+           
+            //hiddenObjectCount++;  // Increment the count when object is hidden
+            //CheckHiddenObjects(); // Check if all objects are hidden
+            
             BlackGun.SetActive(false);
         }
         else
@@ -43,15 +51,65 @@ public class Close_Gun_Villa : MonoBehaviour
         }
     }
 
+    void HideObject()
+    {
+        if (BlackGun != null)
+        {
+
+            //hiddenObjectCount++;  // Increment the count when object is hidden
+            //CheckHiddenObjects(); // Check if all objects are hidden
+
+            BlackGun.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("BlackGun GameObject is not assigned.");
+        }
+
+    }
+
+    IEnumerator CheckHiddenObjects(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (BlackGun != null)
+        {
+            if (hiddenObjectCount == totalHiddenObjects)
+            {
+                // Show the dialogue message
+                if (dialogueText != null)
+                {
+                    dialogueText.text = npcMessage1;
+
+                }
+                else
+                {
+                    Debug.LogError("Text component not found in the Canvas!");
+                }
+
+                dialogueCanvas.enabled = true;
+            }
+            //HideObject();
+        }
+        else
+        {
+            Debug.LogError("BlackGun GameObject is not assigned.");
+        }
+    }
+
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+       if (other.CompareTag("Player"))
         {
             if (dialogueText != null)
             {
                 dialogueText.text = npcMessage;
                 ScoreManager.instance.AddPointEasyMode();
-                StartCoroutine(HideObjectDelayed(3f)); // Delay for 3 seconds
+
+                hiddenObjectCount++;  // Increment the count when object is hidden
+                HideObject();
+
+                StartCoroutine(CheckHiddenObjects(3f)); // Check if all objects are hidden
             }
             else
             {
--- Unity Project/Assets/Close_NPC_Playground.cs
+++ Unity Project/Assets/Close_NPC_Playground.cs
@@ -6,28 +6,48 @@ using TMPro;
 
 public class Close_NPC_Playground : MonoBehaviour
 {
-    public string npcMessage = "Hello there, you are located in the playground park. Your\n"+
+    public string npcMessageEasy = "Hello there, you are located in the playground park. Your\n"+
         "mission is to collect 3 black objects and one traffic sign that are scattered in the\n"+
         "park in order to continue to your next riddle. Good luck!";
 
-    public Canvas dialogueCanvas; // Link this in the Unity Editor
-    private TextMeshProUGUI dialogueText;
+    public string npcMessageMedium = "Hello there, you are located in the playground park. Your\n" +
+       "mission is to collect 4 black objects that are scattered in the\n" +
+       "park in order to continue to your next riddle. Good luck!";
 
-    // hide the medium and hard mode objects
-    
-    // hide medium mode objects
+    public string npcMessageHard = "Hello there, you are located in the playground park. Your\n" +
+        "mission is to collect 5 red objects that are scattered in the\n" +
+        "park in order to continue to your next riddle. Good luck!";
+
+
+    public Canvas dialogueCanvas; // Link this in the Unity Editor
+    public TextMeshProUGUI dialogueText;
+
+    // easy mode objects
+    public GameObject BlackCar;
+    public GameObject BlackWheel;
+    public GameObject BlackSledgeHammer;
+    public GameObject Parkgarage_sign;
+   
+
+    // medium mode objects
     public GameObject BlackWheel1;
     public GameObject BlackPillow;
     public GameObject BlackDonut;
     public GameObject BlackRock;
 
-    // hide hard mode objects
+    // hard mode objects
     public GameObject RedCar;
     public GameObject RedRock;
     public GameObject RedWineGlass;
     public GameObject RedCupcake;
     public GameObject RedFlower;
 
+    
+
+    // Variable to store the last clicked button
+    public static string selectedGameMode;
+
+    private const string GameModeKey = "GameMode";
 
 
     void Start()
@@ -45,7 +65,7 @@ public class Close_NPC_Playground : MonoBehaviour
             dialogueCanvas.enabled = false;
         }
 
-        if (RedCar != null && RedRock != null &&RedWineGlass != null && RedCupcake != null && RedFlower != null
+        /*if (RedCar != null && RedRock != null &&RedWineGlass != null && RedCupcake != null && RedFlower != null
             && BlackWheel1 != null && BlackPillow != null && BlackDonut != null && BlackRock != null)
         {
             RedCar.SetActive(false);
@@ -62,8 +82,89 @@ public class Close_NPC_Playground : MonoBehaviour
         else
         {
             Debug.LogError("Some of your objects  is/are not assigned.");
-        }
+        }*/
 
+        
+        selectedGameMode = PlayerPrefs.GetString(GameModeKey);
+        Debug.Log("Gamemode: " + selectedGameMode);
+
+
+        // depending on mode show and hide the nessesary objects
+        if (selectedGameMode == "EASY")
+        {
+            // show easy mode objects, hide medium and hard mode objects4
+            
+            // show
+            // easy
+            BlackCar.SetActive(true);
+            BlackWheel.SetActive(true);
+            BlackSledgeHammer.SetActive(true);
+            Parkgarage_sign.SetActive(true);
+
+            // hide
+            // medium
+            BlackWheel1.SetActive(false);
+            BlackPillow.SetActive(false);
+            BlackDonut.SetActive(false);
+            BlackRock.SetActive(false);
+
+            // hard
+            RedCar.SetActive(false);
+            RedRock.SetActive(false);
+            RedWineGlass.SetActive(false);
+            RedCupcake.SetActive(false);
+            RedFlower.SetActive(false);
+        }
+        else if (selectedGameMode == "MEDIUM")
+        {
+            // show medium mode objects, hide easy and hard
+
+            // show
+            // medium
+            BlackWheel1.SetActive(true);
+            BlackPillow.SetActive(true);
+            BlackDonut.SetActive(true);
+            BlackRock.SetActive(true);
+
+            // hide
+            // easy
+            BlackCar.SetActive(false);
+            BlackWheel.SetActive(false);
+            BlackSledgeHammer.SetActive(false);
+            Parkgarage_sign.SetActive(false);
+
+            // hard
+            RedCar.SetActive(false);
+            RedRock.SetActive(false);
+            RedWineGlass.SetActive(false);
+            RedCupcake.SetActive(false);
+            RedFlower.SetActive(false);
+        }
+        else if(selectedGameMode == "HARD")
+        {
+            // show hard mode objects, hide easy and medium
+
+            // show
+            // hard
+            RedCar.SetActive(true);
+            RedRock.SetActive(true);
+            RedWineGlass.SetActive(true);
+            RedCupcake.SetActive(true);
+            RedFlower.SetActive(true);
+
+            // hide
+            // easy
+            BlackCar.SetActive(false);
+            BlackWheel.SetActive(false);
+            BlackSledgeHammer.SetActive(false);
+            Parkgarage_sign.SetActive(false);
+
+            // medium
+            BlackWheel1.SetActive(false);
+            BlackPillow.SetActive(false);
+            BlackDonut.SetActive(false);
+            BlackRock.SetActive(false);
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -72,7 +173,12 @@ public class Close_NPC_Playground : MonoBehaviour
         {
             if (dialogueText != null)
             {
-                dialogueText.text = npcMessage;
+                if (selectedGameMode == "EASY")
+                    dialogueText.text = npcMessageEasy;
+                else if(selectedGameMode == "MEDIUM")
+                    dialogueText.text = npcMessageMedium;
+                else if(selectedGameMode == "HARD")
+                    dialogueText.text = npcMessageHard;
             }
             else
             {
@@ -85,6 +191,7 @@ public class Close_NPC_Playground : MonoBehaviour
         {
             Debug.LogError("Error on trigger enter");
         }
+
     }
 
     void OnTriggerExit(Collider other)
