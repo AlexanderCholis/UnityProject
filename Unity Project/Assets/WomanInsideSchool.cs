0807bfe135eb1ab0deb96363689f151828ed3ca3
Alexander Cholis <alexholis2@gmail.com>
2024-03-15 15:44:01 +0200

    NPC interactions inside school

---
+++ Unity Project/Assets/WomanInsideSchool.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WomanInsideSchool : MonoBehaviour
+{
+    public string npcMessage = "You have successfully entered the room!";
+    public Canvas dialogueCanvas; // Link this in the Unity Editor
+    private TextMeshProUGUI dialogueText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueText = dialogueCanvas.transform.Find("Text").GetComponent<TextMeshProUGUI>();
+
+        if (dialogueText == null)
+        {
+            Debug.LogError("Text component not found in the Canvas!");
+        }
+        else
+        {
+            dialogueCanvas.enabled = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (dialogueText != null)
+            {
+                dialogueText.text = npcMessage;
+            }
+            else
+            {
+                Debug.LogError("Text component not found in the Canvas!");
+            }
+
+            dialogueCanvas.enabled = true;
+        }
+        else
+        {
+            Debug.LogError("Error on trigger enter");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dialogueCanvas.enabled = false;
+        }
+        else
+        {
+            Debug.LogError("Error on trigger exit");
+        }
+    }
+}
