f6151ea0dfe331644b810394ad2d6707031d3c4d
Helen Polychroni <p20161@cslab.unipi.gr>
2024-03-16 21:02:59 +0200

    some changes

--- Unity Project/Assets/Close_Player_In_Villa.cs
+++ Unity Project/Assets/Close_Player_In_Villa.cs
@@ -10,13 +10,13 @@ public class Close_Player_In_Villa : MonoBehaviour
 
      public string npcMessageMedium = "Hello there, you have been assigned a very important role.\n" +
         "You are a detective investigating a murder case. Search the house and collect the" +
-        " \n7 objects that incriminate him.The objects are scattered in the different areas" +
-        "of the house.\nHint: We have testimonies that there may be a female captive.\nBe careful and good luck";
+        " \n6 objects that incriminate him.The objects are scattered in the different areas" +
+        "of the house.\nHint: We have testimonies that there may be a female captive.\nBe careful and good luck.";
 
      public string npcMessageHard = "Hello there, you have been assigned a very important role.\n" +
      "You are a detective investigating a murder case. Search the house and collect the" +
-     " \n6 objects that incriminate him.The objects are scattered in the different areas" +
-     "of the house.\nHint: We have testimonies that there may be a female captive.\nBe careful and good luck";
+     " \n8 objects that incriminate him.The objects are scattered in the different areas" +
+     "of the house.\nHint: We have testimonies that there may be a female captive.\nBe careful and good luck.";
 
     public Canvas dialogueCanvas; // Link this in the Unity Editor
     private TextMeshProUGUI dialogueText;
