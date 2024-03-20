5f29f691adc608a1ad6f5bc80ab37388257b8679
Alexander Cholis <alexholis2@gmail.com>
2024-03-20 21:55:53 +0200

    Added NPC message outside Residence

---
+++ Unity Project/Assets/ManOutsideResidence.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Localization;
+
+public class ManOutsideResidence : MonoBehaviour
+{
+    public LocalizedString ManResidenceMessage;
+
+    public static string selectedGameMode;
+
+    private const string GameModeKey = "GameMode";
+
+    /*
+    public string npcMessage = "You have successfully entered the room! \n" +
+        "I'm the substitute teacher. The school teacher is missing. \n" +
+        "Continue to the villa for more information!";
+    */
+    public Canvas dialogueCanvas; // Link this in the Unity Editor
+    private TextMeshProUGUI dialogueText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueText = dialogueCanvas.transform.Find("Text").GetComponent<TextMeshProUGUI>();
+
+        if (dialogueText == null)
+        {
+            Debug.LogError("Text component not found in the Canvas!");
+        }
+        else
+        {
+            dialogueCanvas.enabled = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        selectedGameMode = PlayerPrefs.GetString(GameModeKey);
+
+        if (other.CompareTag("Player"))
+        {
+            if (dialogueText != null)
+            {
+                if (selectedGameMode == "HARD")
+                {
+                    string localizedMessage = ManResidenceMessage.GetLocalizedString();
+                    dialogueText.text = localizedMessage;
+                }
+            }
+            else
+            {
+                Debug.LogError("Text component not found in the Canvas!");
+            }
+
+            dialogueCanvas.enabled = true;
+        }
+        else
+        {
+            Debug.LogError("Error on trigger enter");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dialogueCanvas.enabled = false;
+        }
+        else
+        {
+            Debug.LogError("Error on trigger exit");
+        }
+    }
+}
